3dbb77ab8efb775b22909311443ed4081ffc7e66
NNAAAA <nngguyen.anh@gmail.com>
2021-06-19 20:06:41 +0700

    add comment to restAPI and controller

--- client/api.py
+++ client/api.py
@@ -1,21 +1,29 @@
 import ast
 def signin(userParams,socket):
     userParams["cmd"] = "login"
+    # gửi tài khoản mật khẩu lên server
     socket.send(bytes(str(userParams),'utf8'))
+    # server trả lỗi về
     err = ast.literal_eval(socket.recv(1024).decode('utf8'))
+    # server trả về lỗi tài khoản
     if err["account"]:
         print("😩 Account doesn't exist")
         return False
+    # server trả về lỗi mật khẩu
     elif err["password"]:
         print("😓 Wrong password")
         return False
+    # server trả về lỗi rỗng
     else:  
         return True
 
 def signup(userParams,socket):
     userParams["cmd"] = "register"
+    # gửi tài khoản mật khẩu lên server
     socket.send(bytes(str(userParams),'utf8'))
+    # server trả lỗi về
     err = ast.literal_eval(socket.recv(1024).decode('utf8'))
+    # server trả về lỗi tài khoản 
     if err["account"]:
         print("😛 Account does exist")
         return False
--- server/userController.py
+++ server/userController.py
@@ -4,14 +4,17 @@ db = client["Facebook"]
 
 def postLogin(userParams):
     users = db.users
+    #tìm tài khoản trong database
     user = users.find_one({"account":userParams["account"]})
     err = {
         "account": False,
         "password": False
     }
+    # tài khoảng không tồn tại
     if not user:
         err["account"] = True
         return err
+    # mật khẩu không khớp
     if userParams["password"] != user["password"]:
         err["password"] = True
         return err 
@@ -19,13 +22,16 @@ def postLogin(userParams):
 
 def postRegister(userParams):
     users = db.users
+    #tìm tài khoản trong database
     user = users.find_one({"account":userParams["account"]})
     err = {
         "account": False,
     }
+    # tài khoản tồn tại
     if user:
         err["account"] = True
         return err
     else:
+        #thêm tài khoản vào database
         users.insert_one(userParams)
         return err
